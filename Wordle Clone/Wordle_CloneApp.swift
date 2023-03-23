499cd82f1e7fe2535f39aee2cc757e5e709be6b0
Duke Holsing <dh021804@gmail.com>
2023-03-23 07:57:10 -0400

    Initial Commit

---
+++ Wordle Clone/Wordle_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle_CloneApp.swift
+//  Wordle Clone
+//
+//  Created by Duke on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Wordle_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
